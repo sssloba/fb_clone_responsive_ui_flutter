04a6e011d9524d0d7ec655330b09892c05154974
sssloba <slobodan.stojakov@gmail.com>
2021-11-24 23:43:35 +0100

    -app-nav-screen-tab-bar

---
+++ lib/widgets/custom_tab_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+
+class CustomTabBar extends StatelessWidget {
+  const CustomTabBar({
+    Key key,
+    @required this.icons,
+    @required this.selectedIndex,
+    @required this.onTap,
+  }) : super(key: key);
+
+  final List<IconData> icons;
+  final int selectedIndex;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      indicatorPadding: EdgeInsets.zero,
+      indicator: BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Palette.facebookBlue, width: 3.0),
+        ),
+      ),
+      tabs: icons
+          .asMap()
+          .map(
+            (i, e) => MapEntry(
+              i,
+              Tab(
+                icon: Icon(
+                  e,
+                  color: i == selectedIndex
+                      ? Palette.facebookBlue
+                      : Colors.black45,
+                  size: 30.0,
+                ),
+              ),
+            ),
+          )
+          .values
+          .toList(),
+      onTap: onTap,
+    );
+  }
+}
